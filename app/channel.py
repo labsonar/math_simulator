905c838866ec731785a5311ee23aea2617cf937c
fabio <fabio.oliveira@lps.ufrj.br>
2025-03-25 06:55:07 -0300

    Ajustando exemplo de propag. canal

--- app/channel.py
+++ app/channel.py
@@ -5,23 +5,24 @@ import lps_synthesis.propagation.channel_description as lps_desc
 import lps_synthesis.propagation.channel as lps_channel
 
 
-lps_channel.TEMP_DEFAULT_DIR = "./lps_libraries/acoustic_synthesis/result/propagation"
-
 desc = lps_desc.Description()
 desc.add(lps_qty.Distance.m(0), lps_qty.Speed.m_s(1500))
 desc.add(lps_qty.Distance.m(50), lps_layer.BottomType.CHALK)
 
+source_depths = [lps_qty.Distance.m(10)]
+
 ch = lps_channel.Channel(description = desc,
                 sensor_depth = lps_qty.Distance.m(40),
+                source_depths = source_depths,
                 max_distance = lps_qty.Distance.km(1),
                 max_distance_points = 128,
                 sample_frequency = lps_qty.Frequency.khz(16),
                 frequency_range=[lps_qty.Frequency.khz(5), lps_qty.Frequency.khz(5.05)],
-                temp_dir=lps_channel.TEMP_DEFAULT_DIR,
+                temp_dir = "./lps_libraries/acoustic_synthesis/result/propagation",
                 model = lps_model.Model.OASES)
 
-# ch.propagate(input_data: np.array,
-#              source_depth: lps_qty.Distance,
-#              distance: typing.List[lps_qty.Distance])
+# propagated_signal = ch.propagate(input_data = np.array(...),
+#              source_depth = source_depths[0],
+#              distance = lps_qty.Distance.m(200))
 
 print(ch.description)
